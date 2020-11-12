60dc558f91344a1876c43252ceba3590b97a1909
anhquan291 <anhquan291@gmail.com>
2020-11-12 18:14:38 +0700

    initial

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({ component: Component, path, ...props }) => {
+  const user = useSelector((state) => state.auth.user);
+  const userLocal = localStorage.getItem('user');
+  const isAuthenticated = user !== null || userLocal !== null ? true : false;
+  if (!isAuthenticated) {
+    return <Navigate to='/login' />;
+  }
+  return <Route {...props} path={path} element={<Component />} />;
+};
+
+export default PrivateRoute;
---
+++ src/routes/routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+const routes = [{}];
+export default routes;
